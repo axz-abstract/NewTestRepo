6f59a9100441c9d778d8d7a3f21ee5fa035ad7f4
Axel Abstracta <axel.arzuaga@abstracta.com.uy>
2024-02-05 17:20:42 -0300

    First commit!

---
+++ hello/HolaMundo.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class HolaMundo {
+
+    public static void main(String[] args){
+        for(int i=0;i<10;i++){
+            System.out.println("Hola Mundo --- " + i);
+        }
+    }
+}
